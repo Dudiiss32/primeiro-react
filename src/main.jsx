2471920467ec21900f251586e3ba81f86e4f241f
Maria Eduarda Weber <mariaeduardaweber509@gmail.com>
2025-08-28 11:14:05 -0300

    novos componentes e useState

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import CaixaTexto from './caixaTexto.jsx'
 import Paragrafo from './paragrafo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Paragrafo/>
+    <Paragrafo nome='Duda'/> 
+    {/* estou criando uma property chamada nome e passando para o componente parágrafo*/}
+    <Paragrafo nome='Bighouse'/> 
+    <Paragrafo nome='CU'/> 
+    <CaixaTexto></CaixaTexto>
+
   </StrictMode>,
 )
--- src/paragrafo.jsx
+++ src/paragrafo.jsx
@@ -1,5 +1,29 @@
-function Paragrafo(){
-    return <p>Olá! Eu sou um parágrafo.</p>
+// componente paragrafo
+import { useState } from 'react'
+// importando o useState de dentro da biblio react
+
+const Paragrafo = (props) => {
+    const [pontos, setPontos] = useState(0)
+    // retorna o 0 q vai ser colocado nos pontos e ele mesmo
+
+    const aumentarPontos = () => {
+        setPontos(pontos + 1)
+    }
+    const diminuirPontos = () => {
+        setPontos((pontos == 0) ? 0 : pontos - 1)
+        // return (pontos == 0) ? 0 : setPontos(pontos -1)
+        
+    }
+
+
+    return (
+        <div>
+            <p>Olá! Seja bem-vindo {props.nome}.</p>
+            <p>{props.nome} tem {pontos} pontos. <button onClick={aumentarPontos}>+</button> <button onClick={diminuirPontos}>-</button></p>
+            
+        </div>
+    )
+    // em props tem q usar {props.nomeDaProp}
 }
 
 export default Paragrafo 
